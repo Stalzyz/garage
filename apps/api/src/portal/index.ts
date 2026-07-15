7b66be1e83e786c8a0ca92785327fe0cdf880d4b
Stalzyz <mstalinkumar05@gmail.com>
2026-07-15 18:06:08 +0530

    fix: apply requireAuth middleware to portal routes to populate req.user

--- apps/api/src/portal/index.ts
+++ apps/api/src/portal/index.ts
@@ -2,5 +2,6 @@ import { FastifyInstance } from 'fastify';
 import portalRouter from './portal.router';
 
 export default async function portalModule(app: FastifyInstance) {
+  app.addHook("preHandler", app.requireAuth);
   app.register(portalRouter);
 }
