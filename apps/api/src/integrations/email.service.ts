5977fbc6f692e1fde3c03c9f1a8126716f37c3f9
Stalzyz <mstalinkumar05@gmail.com>
2026-07-25 19:08:27 +0530

    Fix secure flag for SMTP in integrations

--- apps/api/src/integrations/email.service.ts
+++ apps/api/src/integrations/email.service.ts
@@ -35,7 +35,6 @@ async function getTransporter(): Promise<{ transporter: nodemailer.Transporter; 
     transporter = nodemailer.createTransport({
       host,
       port,
-      secure,
       auth: { user, pass },
     });
   } else {
