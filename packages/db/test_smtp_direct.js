c51c66afa1c781fab552332b6f4428afaaf867b2
Stalzyz <mstalinkumar05@gmail.com>
2026-08-14 19:36:36 +0530

    Add SMTP direct test script

---
+++ packages/db/test_smtp_direct.js
@@ -0,0 +1,54 @@
+const nodemailer = require("nodemailer");
+
+async function run() {
+  const host = "smtp.hostinger.com";
+  const port = 465;
+  const user = "admin@grekam.in";
+  const pass = "Medusa09#";
+
+  console.log("Testing with secure: true...");
+  try {
+    const transporter = nodemailer.createTransport({
+      host,
+      port,
+      secure: true,
+      auth: { user, pass },
+      debug: true,
+      logger: true
+    });
+
+    const info = await transporter.sendMail({
+      from: '"Team Grekam" <admin@grekam.in>',
+      to: "mstalinkumar05@gmail.com",
+      subject: "Test Direct SMTP SSL",
+      html: "<b>SSL Test</b>"
+    });
+    console.log("Success with secure: true!", info);
+  } catch (err) {
+    console.error("Failed with secure: true:", err);
+  }
+
+  console.log("\nTesting with secure: false...");
+  try {
+    const transporter = nodemailer.createTransport({
+      host,
+      port,
+      secure: false,
+      auth: { user, pass },
+      debug: true,
+      logger: true
+    });
+
+    const info = await transporter.sendMail({
+      from: '"Team Grekam" <admin@grekam.in>',
+      to: "mstalinkumar05@gmail.com",
+      subject: "Test Direct SMTP non-SSL",
+      html: "<b>Non-SSL Test</b>"
+    });
+    console.log("Success with secure: false!", info);
+  } catch (err) {
+    console.error("Failed with secure: false:", err);
+  }
+}
+
+run();
